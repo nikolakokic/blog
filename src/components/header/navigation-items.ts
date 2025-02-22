130231c4c34407fe62919ac87d92f0eeb0989d9d
nikolakokic <nikola.it.955@gmail.com>
2025-02-22 18:54:51 +0100

    Add initial files and finish header

---
+++ src/components/header/navigation-items.ts
@@ -0,0 +1,25 @@
+import { NavItem } from "./header.types";
+
+
+export const NAVIGATION: NavItem[] = [
+  {
+    path: '/',
+    label: 'Home'
+  },
+  {
+    path: '/blog',
+    label: 'Blog'
+  },
+  {
+    path: '/single-post',
+    label: 'Single Post'
+  },
+  {
+    path: '/pages',
+    label: 'Pages'
+  },
+  {
+    path: '/contact',
+    label: 'Contact'
+  }
+];
